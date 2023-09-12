62803ac8f0c3e62ebfe8cf9f8bd3d7a5a51f38ef
Brady Phelps <bp309420@ohio.edu>
2023-09-11 23:13:35 -0400

    Added template

---
+++ Color-Theory-Vintage/Color_Theory_VintageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Theory_VintageApp.swift
+//  Color-Theory-Vintage
+//
+//  Created by Brady Phelps on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Color_Theory_VintageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
