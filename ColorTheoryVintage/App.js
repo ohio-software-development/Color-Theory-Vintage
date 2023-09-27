c93d7f0ff9a73ef8509c67fa6011bea5cb683daa
SoahLi <ows8turnbull@gmail.com>
2023-09-27 09:34:21 -0400

    update

--- ColorTheoryVintage/App.js
+++ ColorTheoryVintage/App.js
@@ -1,46 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from "./src/Screens/home";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-const Stack = createStackNavigator();
-
-//default theme for NavigationContainer???
-//fixes problem with screen background not being white
-const navTheme = DefaultTheme;
-navTheme.colors.background = 'white';
-
-function MyStack() {
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import MyTabs from "./navigation/tabs";
+const App = () => {
   return (
-    <Stack.Navigator >
-      <Stack.Screen options={{headerShown: false}} name="Home" component={Home} />
-    </Stack.Navigator>
-  );
-}
-
-export default function App() {
-  return (
-    <NavigationContainer
-      screenOptions={{
-        headerShown: false
-      }}
-    >
-      <MyStack />
+    <NavigationContainer>
+      <MyTabs/>
     </NavigationContainer>
   );
 }
+export default App;
 
-
-
-
-/* 
-import { initializeApp } from 'firebase/app';
-
-// TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-  //...
-};
-
-const app = initializeApp(firebaseConfig);
-*/
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
--- README.md
+++
@@ -1,5 +0,0 @@
-# Color-Theory-Vintage
-Collaboration with ColorTheoryVintage
-
-# React-Native Branch
-<p>For info on how to run npm/npx to install correct node modules for deployment, see <a href = "https://reactnative.dev/docs/environment-setup?guide=quickstart">the docs</a>.  It is super easy to follow and takes maybe two commands.  For help feel free to reach out. :)</p>
